83995752537aeab3647e5f1f85de76e8828a8cda
Hackbright Student <info@hackbrightacademy.com>
2016-04-12 16:40:49 -0700

    Refactored make chains function

--- markov.py
+++ markov.py
@@ -10,11 +10,9 @@ def open_and_read_file(file_path):
 
     with open(file_path) as f:
         file_contents = f.read()
-        #our thought is to do this later as we iterate through the string
-        #file_contents.strip('\n')
-
-    # return "This should be a variable that contains your file text as one long string"
+      
     return file_contents
+
 
 def make_chains(text_string):
     """Takes input text as string; returns _dictionary_ of markov chains.
@@ -34,23 +32,11 @@ def make_chains(text_string):
     words = text_string.split()
 
    
-    for index in range(len(words)):
+    for index in range(len(words) -2):
         
-        if index == len(words) - 2:
-            break
-        else:
-            if (words[index], words[index + 1]) not in chains.keys():
-                chains[(words[index], words[index + 1])] = [(words[index + 2])]
-            else:
-                existing_values = chains[(words[index], words[index + 1])]
-                existing_values.append(words[index + 2])
-                chains[(words[index], words[index + 1])] = existing_values
+        word_pair_key = (words[index], words[index + 1])
 
-                # doesnt work and we dont know why:
-                # chains[(words[index], words[index + 1])] = chains[(words[index], 
-                #     words[index + 1])].append(words[index + 2])
-            
-            #old idea: word_pairs.append((words[index], words[index + 1]))
+        chains[word_pair_key] = chains.get(word_pair_key, []) + [words[index + 2]]
     
     return chains
 
@@ -59,19 +45,18 @@ def make_chains(text_string):
 def make_text(chains):
     """Takes dictionary of markov chains; returns random text."""
 
-    text = ""
-
     starting_key = choice(chains.keys())
     text = starting_key[0] + ' ' + starting_key[1]
     #split here first
-    split_text =text.split()
+    split_text = text.split()
 
     #start while loop
     while chains.get((split_text[-2],split_text[-1]), None):
         
-        text = text + ' ' + choice(chains[(split_text[-2],split_text[-1])])
-        split_text =text.split()
-       
+        new_word = choice(chains[(split_text[-2],split_text[-1])])
+
+        text = text + ' ' + new_word
+        split_text.append(new_word)
 
     return text
 
