b3dc102e32db377b626bb2bdf8a224f99b984da2
Hackbright Student <info@hackbrightacademy.com>
2016-04-13 12:41:52 -0700

    working code for key length n, passed in from command line

--- markov.py
+++ markov.py
@@ -30,33 +30,15 @@ def make_chains(text_string, num):
         {('hi', 'there'): ['mary', 'juanita'], ('there', 'mary'): ['hi'], ('mary', 'hi': ['there']}
     """
 
-    # chains = {}
-
-    # words = text_string.split()
-
-   
-    # for index in range(len(words) - 2):
-        
-    #     word_pair_key = (words[index], words[index + 1])
-
-    #     chains[word_pair_key] = chains.get(word_pair_key, []) + [words[index + 2]]
-    
-    # return chains
-
     chains = {}
 
     words = text_string.split()
 
    
     for index in range(len(words) - num):
-        # word_key = []
 
-        # for i in range(num):
-        #     word_key.append(words[i])
-
-        word_key = tuple(words[index:index + num]) #for i in range(num)])
-
-        # word_key = tuple(word_key)
+        #create dictionary key tuple from a slice the length of passed num
+        word_key = tuple(words[index:index + num])
 
         chains[word_key] = chains.get(word_key, []) + [words[index + num]]
     
@@ -68,18 +50,12 @@ def make_text(chains, num):
     """Takes dictionary of markov chains; returns random text."""
 
     starting_key = choice(chains.keys())
-    #text = starting_key[0] + ' ' + starting_key[1]
+
     text = ' '.join(starting_key)
     #split here first
     split_text = text.split()
 
-    #start while loop
-
-    #new_key = (split_text[-2],split_text[-1])
-
-    #while chains.get((split_text[-2],split_text[-1]), None):
     while chains.get(tuple(split_text[-num:]), None):
-    #while chains.get(new_key, None):
         
         new_word = choice(chains[tuple(split_text[-num:])])
 
@@ -89,20 +65,21 @@ def make_text(chains, num):
     return text
 
 
-input_path = "green-eggs.txt"
+# input_path = "green-eggs.txt"
 # input_path = "gettysburg.txt"
-# input_path = sys.argv[1]
+input_path = sys.argv[1]
+key_length = sys.argv[2]
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
 
 # Get a Markov chain
 chains = make_chains(input_text,3)
-print chains
+# print chains
 
 # Produce random text
 random_text = make_text(chains, 3)
 
-# print random_text
+print random_text
 
 
