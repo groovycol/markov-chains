25ed83c7a81359f586a9cf9d968f14230a83555a
Hackbright Student <info@hackbrightacademy.com>
2016-04-13 12:35:18 -0700

    added functionality to specify the length of the dictionary key tuple.

--- markov.py
+++ markov.py
@@ -16,46 +16,72 @@ def open_and_read_file(file_path):
     return file_contents
 
 
-def make_chains(text_string):
-    """Takes input text as string; returns _dictionary_ of markov chains.
+def make_chains(text_string, num):
+    """Takes input text as string as well as an integer for dictionary key length; 
+    returns _dictionary_ of markov chains.
 
-    A chain will be a key that consists of a tuple of (word1, word2)
-    and the value would be a list of the word(s) that follow those two
+    A chain will be a key that consists of a tuple of (word1, word2,... wordn)
+    and the value would be a list of the word(s) that follow those
     words in the input text.
 
     For example:
 
-        >>> make_chains("hi there mary hi there juanita")
+        >>> make_chains("hi there mary hi there juanita",2)
         {('hi', 'there'): ['mary', 'juanita'], ('there', 'mary'): ['hi'], ('mary', 'hi': ['there']}
     """
+
+    # chains = {}
+
+    # words = text_string.split()
+
+   
+    # for index in range(len(words) - 2):
+        
+    #     word_pair_key = (words[index], words[index + 1])
+
+    #     chains[word_pair_key] = chains.get(word_pair_key, []) + [words[index + 2]]
+    
+    # return chains
 
     chains = {}
 
     words = text_string.split()
 
    
-    for index in range(len(words) - 2):
-        
-        word_pair_key = (words[index], words[index + 1])
+    for index in range(len(words) - num):
+        # word_key = []
 
-        chains[word_pair_key] = chains.get(word_pair_key, []) + [words[index + 2]]
+        # for i in range(num):
+        #     word_key.append(words[i])
+
+        word_key = tuple(words[index:index + num]) #for i in range(num)])
+
+        # word_key = tuple(word_key)
+
+        chains[word_key] = chains.get(word_key, []) + [words[index + num]]
     
     return chains
 
 
 
-def make_text(chains):
+def make_text(chains, num):
     """Takes dictionary of markov chains; returns random text."""
 
     starting_key = choice(chains.keys())
-    text = starting_key[0] + ' ' + starting_key[1]
+    #text = starting_key[0] + ' ' + starting_key[1]
+    text = ' '.join(starting_key)
     #split here first
     split_text = text.split()
 
     #start while loop
-    while chains.get((split_text[-2],split_text[-1]), None):
+
+    #new_key = (split_text[-2],split_text[-1])
+
+    #while chains.get((split_text[-2],split_text[-1]), None):
+    while chains.get(tuple(split_text[-num:]), None):
+    #while chains.get(new_key, None):
         
-        new_word = choice(chains[(split_text[-2],split_text[-1])])
+        new_word = choice(chains[tuple(split_text[-num:])])
 
         text = text + ' ' + new_word
         split_text.append(new_word)
@@ -63,19 +89,20 @@ def make_text(chains):
     return text
 
 
-# input_path = "green-eggs.txt"
+input_path = "green-eggs.txt"
 # input_path = "gettysburg.txt"
-input_path = sys.argv[1]
+# input_path = sys.argv[1]
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
 
 # Get a Markov chain
-chains = make_chains(input_text)
+chains = make_chains(input_text,3)
+print chains
 
 # Produce random text
-random_text = make_text(chains)
+random_text = make_text(chains, 3)
 
-print random_text
+# print random_text
 
 
